8b0e546fbb94797bd2d865bb7d398de0f75aa8c1
Yan Hernandez <yanhernandez.1997@gmail.com>
2020-11-14 16:23:52 -0500

    ok

--- Procfile
+++ Procfile
@@ -1,4 +1,1 @@
-web: npm install
-web: rimraf dist
-web: npm run build
 web: npm run start:prod
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { HttExceptionFilter } from './shared/http-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttExceptionFilter());
+  app.enableCors();
   await app.listen(process.env.PORT || 3000);
 }
 
